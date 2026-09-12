9b20c4db342c99b69ff6f7a4705a853486964fa5
abhilashbattula <vk970278@gmail.com>
2026-09-12 11:05:31 +0530

    workspace updated

--- ex2.py
+++ ex2.py
@@ -4,9 +4,4 @@ class Math:
         self.b=b
     def add(self):
         print(self.a+self.b)
-    def mul(self):
-        self.add()    
-        print(self.a*self.b)
-if __name__=="__main__":
-    Math(20,30).add()
-    Math(100,200).mul()
+    
